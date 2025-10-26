cfbc9bef6dce0be8399b3111190fe9a3ebfb4f4d
buildwithfousiya <fousiya9198@gmail.com>
2025-10-26 21:05:29 +0530

    Practices

---
+++ practice/first.js
@@ -0,0 +1,5 @@
+console.log("Hey");
+FullName = "Fousiya";
+console.log(FullName);
+x = null;
+console.log(null);
---
+++ practice/fourth.js
@@ -0,0 +1,8 @@
+let gameNum = 25;
+let userNum = Number(prompt("Guess the game number: "));
+
+while(userNum !== gameNum) {
+    userNum = Number(prompt("You entered wrong number, Guess again: "));
+}
+
+console.log("Congratulations!, you entered the right number");
