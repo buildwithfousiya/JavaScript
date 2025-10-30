dd8f0104640a793816cb7d59fafa2b282eda179c
buildwithfousiya <fousiya9198@gmail.com>
2025-10-30 20:28:56 +0530

    practices

--- fourteen.js
+++ fourteen.js
@@ -1,1 +1,9 @@
-  
+let h2 = document.querySelector("h2");
+console.dir(h2.innerText);
+
+h2.innerText = h2.innerText + " JavaScript!";
+
+
+let divs = document.querySelectorAll(".box");
+console.log(divs);
+divs[0].innerText = "new unique value";
